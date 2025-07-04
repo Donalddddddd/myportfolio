81a200d7585601acf27dd3a90012ae501944b771
Donalddddddd <161224579+Donalddddddd@users.noreply.github.com>
2025-07-04 18:22:10 +0800

    updated

--- asssets/main.js
+++ asssets/main.js
@@ -2,7 +2,7 @@ function myMenuFunction(){
     var menuBtn = document.getElementById("myNavMenu");
 
     if(menuBtn.className === "nav-menu"){
-            menuBtn.className -= " responsive";
+            menuBtn.className += " responsive";
     }else{
             menuBtn.className = "nav-menu";
     }
