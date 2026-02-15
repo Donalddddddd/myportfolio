de6aad305e5994d7a4f4dbc7787d59ef6e994fb2
Donalddddddd <161224579+Donalddddddd@users.noreply.github.com>
2026-02-15 15:53:17 +0800

    remove show less/more

--- asssets/main.js
+++ asssets/main.js
@@ -175,24 +175,6 @@ function scrollActive() {
 }
 window.addEventListener('scroll', scrollActive)
 
-function toggleProjects() {
-    const hiddenProjects = document.querySelectorAll('.hidden-project');
-    const btn = document.getElementById('toggleProjectsBtn');
-    
-    // Check if we are currently showing or hiding
-    if (btn.innerText === "Show More") {
-        hiddenProjects.forEach(project => {
-            project.style.display = "block";
-        });
-        btn.innerText = "Show Less";
-    } else {
-        hiddenProjects.forEach(project => {
-            project.style.display = "none";
-        });
-        btn.innerText = "Show More";
-    }
-}
-
 function themeFunction() {
     const body = document.body;
     const themeButton = document.getElementById("theme-button");
